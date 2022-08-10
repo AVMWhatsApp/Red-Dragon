8e26b227900fa48a5aef6ed09dfa7ed35f17e3f8
AVMWhatsApp <110322160+AVMWhatsApp@users.noreply.github.com>
2022-08-10 16:41:22 +0500

    Add files via upload

---
+++ comandos.js
@@ -0,0 +1,18 @@
+
+🍃COMANDOS🍃 
+
+> pkg update && pkg upgrade
+> pkg install git -y
+> pkg install nodejs -y
+> pkg install ffmpeg -y
+> pkg install imagemagick -y
+> termux-setup-storage
+> git clone https://github.com/Yovanihades1212/HADES-BOT-MD.git
+> cd HADES-BOT-MD
+> npm install
+> pkg install yarn
+> npm update
+> yarn
+> npm start
+
+🤖SUSCRÍBETE AL CANAL 💖
